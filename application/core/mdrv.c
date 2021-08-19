1501c6420b47d631e576d4152e2e10e93f8e5be9
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2021-08-19 23:02:11 +0200

    chore: added timeout and code commenting
    Signed-off-by: Nenad Radulovic <nenad.b.radulovic@gmail.com>

--- application/core/mdrv.c
+++ application/core/mdrv.c
@@ -8,6 +8,7 @@
 #include <string.h>
 
 #include "mdrv.h"
+#include "poll_timer.h"
 #include "generic/nk_debug.h"
 #include "generic/nk_array.h"
 #include "generic/nk_types.h"
@@ -17,9 +18,23 @@
 #define IO_BUFFER_LENGTH                2048u
 #define MIN_PRE_TX_PERIODS              10u
 
+/*
+ * Enable debugging of RX or TX processes
+ *
+ * When you enable RX or TX debug you will be able to see each state start/stop
+ * on STATUS GPIO pin.
+ */
 #define DEBUG_STATUS__TX                0
-#define DEBUG_STATUS__RX                0
-#define DEBUG_IGNORE_LAST_BIT           1
+#define DEBUG_STATUS__RX                1
+
+/*
+ * This is an errata of the anyCS chip which was detected in some cases.
+ * 
+ * It was noticed that anyCS will generate invalid Manchester (both half-bits 
+ * are 1-1) code if last bit is '1'. The workaround for this is to ignore last
+ * half bit and always generate it based on the first half-bit.
+ */
+#define ERRATA_IGNORE_LAST_BIT           1
 
 #if ((DEBUG_STATUS__RX == 1) || (DEBUG_STATUS__TX == 1))
 #include "stm32f4xx.h"
@@ -42,13 +57,16 @@ static struct mdrv__exc_buffer_bucket g__rx__buffer =
 static struct bit_buffer g__bit_buffer = NK_ARRAY__BUCKET_INITIALIZER_EMPTY(&g__bit_buffer)
 ;
 
-static const struct mdrv__config default_config = {
+/*
+ * Default runtime configuration
+ */
+static const struct mdrv__config g__default_config = {
     .idle = MDRV__CONFIG__IDLE__FLOAT,
     .pre_tx_state = MDRV__CONFIG__PRE_TX__FLOAT,
     .pre_tx_period_count = 512u,
     .quarter_period_us = 4u};
 
-static void set_idle(const struct mdrv__context *context)
+static void set_gpio_to_idle(const struct mdrv__context *context)
 {
     switch (context->p__config->idle) {
     case MDRV__CONFIG__IDLE__HIGH:
@@ -68,8 +86,9 @@ void mdrv__init(struct mdrv__context *context, const struct mdrv__ll *ll, void *
     memset(context, 0, sizeof(*context));
     memcpy(&context->p__ll, ll, sizeof(context->p__ll));
     context->p__ll_context = ll_context;
-    context->p__config = &default_config;
-    set_idle(context);
+    context->p__config = &g__default_config;
+    set_gpio_to_idle(context);
+    poll_timer__init();
 }
 
 int mdrv__set_config(struct mdrv__context *context, const struct mdrv__config *config)
@@ -78,7 +97,7 @@ int mdrv__set_config(struct mdrv__context *context, const struct mdrv__config *c
         return -1;
     }
     context->p__config = config;
-    set_idle(context);
+    set_gpio_to_idle(context);
     return 0;
 }
 
@@ -95,6 +114,8 @@ enum nk_error mdrv__xchg(struct mdrv__context *context,
                          size_t rx_size)
 {
     struct nk_manchester__result mnc_result;
+
+    HAL_GPIO_WritePin(MCP_STATUS_GPIO_PORT, MCP_STATUS_PIN, GPIO_PIN_RESET);
 
     nk_assert(((rx_size != 0u) && (rx_data != NULL)) || ((rx_size == 0u) && (rx_data == NULL)));
 
@@ -138,19 +159,23 @@ enum nk_error mdrv__xchg(struct mdrv__context *context,
         context->p__state = MDRV__STATE__INIT_TX;
     }
     context->p__ll.tim_start(context->p__ll_context, context->p__config->quarter_period_us);
+    /*
+     * Wait until ISR finishes its job
+     */
     while (context->p__state != MDRV__STATE__IDLE) {
         ;
     }
-#if (DEBUG_IGNORE_LAST_BIT == 1)
+#if (ERRATA_IGNORE_LAST_BIT == 1)
     /*
-     * When we have this enabled, we always force the last bit to be opposite of the bit before the
-     * last one.
+     * When we have this enabled, we always force the last half-bit to be opposite of the 
+     * half-bit before the last one.
      */
     if (g__rx__buffer.array.length >= 2u) {
         g__rx__buffer.array.items[g__rx__buffer.array.length - 1u] =
                             !g__rx__buffer.array.items[g__rx__buffer.array.length - 2u];
     }
 #endif
+    /* See if everything went OK */
     if ((rx_size != 0u) && (rd_data != NULL)) {
         mnc_result = nk_manchester__decode__biphasel(&g__rx__buffer.array, rd_data);
 
@@ -171,12 +196,18 @@ enum nk_error mdrv__xchg(struct mdrv__context *context,
     return 0;
 }
 
+/*
+ * Force compiler optimization of this function regardles of firmware wide
+ * setting.
+ */
 __attribute__ ((optimize(3)))
 void mdrv__it(struct mdrv__context *context)
 {
     switch (context->p__state) {
+    /* Idle state */
     case MDRV__STATE__IDLE:
         break;
+    /* State PRE_TX */
     case MDRV__STATE__PRE_TX:
         switch (context->p__config->pre_tx_state) {
         case MDRV__CONFIG__PRE_TX__HIGH:
@@ -194,6 +225,11 @@ void mdrv__it(struct mdrv__context *context)
         }
         context->p__state = MDRV__STATE__PRE_TX_COUNT;
         break;
+    /* 
+     * In this state we are waiting before starting data transmission.
+     * 
+     * This wait is controlled by p__period_count member.
+     */
     case MDRV__STATE__PRE_TX_COUNT:
         context->p__period_count--;
         if (context->p__period_count == 0u) {
@@ -205,43 +241,29 @@ void mdrv__it(struct mdrv__context *context)
          * Write the first bit together with initialisation. Since this is the first bit, we don't
          * need to check if we have reached end of transmission like we do in MDRV__STATE__TX
          */
-#if (DEBUG_STATUS__RX == 1) && (DEBUG_STATUS__TX == 0)
-        HAL_GPIO_WritePin(MCP_STATUS_GPIO_PORT, MCP_STATUS_PIN, GPIO_PIN_SET);
-#endif
-#if (DEBUG_STATUS__TX == 1)
-        HAL_GPIO_WritePin(MCP_STATUS_GPIO_PORT, MCP_STATUS_PIN, GPIO_PIN_SET);
-#endif
         context->p__ll.pin_init_output(context->p__ll_context,
                                        g__tx__buffer.array.items[context->p__tx_index++]);
         context->p__state = MDRV__STATE__TX_FHI;
         break;
+    /* Send first half-bit */
     case MDRV__STATE__TX_FH:
-#if (DEBUG_STATUS__TX == 1)
-        HAL_GPIO_WritePin(MCP_STATUS_GPIO_PORT, MCP_STATUS_PIN, GPIO_PIN_SET);
-#endif
         context->p__ll.pin_write(context->p__ll_context,
                                  g__tx__buffer.array.items[context->p__tx_index++]);
         context->p__state = MDRV__STATE__TX_FHI;
         break;
+    /* Send first half-bit idle period */
     case MDRV__STATE__TX_FHI:
-#if (DEBUG_STATUS__TX == 1)
-        HAL_GPIO_WritePin(MCP_STATUS_GPIO_PORT, MCP_STATUS_PIN, GPIO_PIN_RESET);
-#endif
         context->p__state = MDRV__STATE__TX_SH;
         break;
+    /* Send second half-bit */
     case MDRV__STATE__TX_SH: {
-#if (DEBUG_STATUS__TX == 1)
-        HAL_GPIO_WritePin(MCP_STATUS_GPIO_PORT, MCP_STATUS_PIN, GPIO_PIN_SET);
-#endif
         context->p__ll.pin_write(context->p__ll_context,
                                  g__tx__buffer.array.items[context->p__tx_index++]);
         context->p__state = MDRV__STATE__TX_SHI;
         break;
     }
+    /* Send second half-bit idle period */
     case MDRV__STATE__TX_SHI:
-#if (DEBUG_STATUS__TX == 1)
-        HAL_GPIO_WritePin(MCP_STATUS_GPIO_PORT, MCP_STATUS_PIN, GPIO_PIN_RESET);
-#endif
         if (context->p__tx_index == g__tx__buffer.array.length) {
             context->p__state = MDRV__STATE__TX_COMPLETE;
         } else {
@@ -252,7 +274,7 @@ void mdrv__it(struct mdrv__context *context)
 #if (DEBUG_STATUS__RX == 1) && (DEBUG_STATUS__TX == 0)
         HAL_GPIO_WritePin(MCP_STATUS_GPIO_PORT, MCP_STATUS_PIN, GPIO_PIN_RESET);
 #endif
-        set_idle(context);
+        set_gpio_to_idle(context);
         if (context->p__rx_size == 0u) {
             context->p__ll.tim_stop(context->p__ll_context);
             context->p__state = MDRV__STATE__IDLE;
@@ -261,12 +283,34 @@ void mdrv__it(struct mdrv__context *context)
         }
         break;
     case MDRV__STATE__PRE_RX:
+#if (DEBUG_STATUS__RX == 1)
+        HAL_GPIO_WritePin(MCP_STATUS_GPIO_PORT, MCP_STATUS_PIN, GPIO_PIN_SET);
+#endif
         context->p__ll.tim_stop(context->p__ll_context);
         context->p__state = MDRV__STATE__RX_FHI;
         context->p__ll.pin_init_input(context->p__ll_context);
-        while (context->p__ll.pin_read(context->p__ll_context))
-            ;
+        /* Before receiving data we want to detect high-to-low transition.
+         * 
+         * Since we want fast response, we do the waiting in tight loop where we
+         * wait for low state on GPIO pin. In case of faulty anyCS (or an
+         * errata) this is the place to check if we got stuck here.
+         */
+        {
+            bool has_timedout = false;
+            poll_timer__setup_ms(100);
+            while (context->p__ll.pin_read(context->p__ll_context) && !has_timedout) {
+                has_timedout = poll_timer__is_done();
+            }
+            if (has_timedout) {
+                set_gpio_to_idle(context);
+                context->p__state = MDRV__STATE__IDLE;
+            }
+            poll_timer__cleanup();
+        }
         context->p__ll.tim_start(context->p__ll_context, context->p__config->quarter_period_us);
+#if (DEBUG_STATUS__RX == 1)
+        HAL_GPIO_WritePin(MCP_STATUS_GPIO_PORT, MCP_STATUS_PIN, GPIO_PIN_RESET);
+#endif
         break;
     case MDRV__STATE__RX_FHI: {
 #if (DEBUG_STATUS__RX == 1)
@@ -297,14 +341,16 @@ void mdrv__it(struct mdrv__context *context)
         HAL_GPIO_WritePin(MCP_STATUS_GPIO_PORT, MCP_STATUS_PIN, GPIO_PIN_RESET);
 #endif
         if (g__rx__buffer.array.length == context->p__rx_size) {
-            set_idle(context);
+            set_gpio_to_idle(context);
             context->p__ll.tim_stop(context->p__ll_context);
             context->p__state = MDRV__STATE__IDLE;
         } else {
             context->p__state = MDRV__STATE__RX_FHI;
         }
         break;
+    /* Invalid state, this should never happen */
     default:
         break;
     }
 }
+
