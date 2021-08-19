591939827dc62dbad0a0ec03046aea07c74d1e1d
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2021-08-19 22:59:53 +0200

    feat: using application config for version handling
    Signed-off-by: Nenad Radulovic <nenad.b.radulovic@gmail.com>

--- application/core/command_help.c
+++ application/core/command_help.c
@@ -5,27 +5,32 @@
  *      Author: nenad
  */
 
-#include "generic/nk_string.h"
 #include "command_help.h"
+#include "generic/nk_string.h"
+#include "config_application.h"
 
 const char* command_help__fn(void *terminal_context,
-                            void *command_context,
-                            struct terminal_arguments *args,
-                            struct nk_string *output)
+                             void *command_context,
+                             struct terminal_arguments *args,
+                             struct nk_string *output)
 {
     (void) terminal_context;
     (void) command_context;
     (void) args;
 
-    nk_string__append_literal(
-            output,
-            NK_STRING__LITERAL(
-                            "\r\n## Manchester Code Proxy (MCP) V1.0 (2021-03-18) command list ##\r\n"
-                            "\r\n"
-                            "help   - Prints the list of available commands\r\n"
-                            "set    - Set a value in MCP configuration\r\n"
-                            "get    - Get a value from MCP configuration\r\n"
-                            "rxchg  - Raw Exchange Data command\r\n"));
+    nk_string__append_literal(output,
+                              NK_STRING__LITERAL(
+                                  "\r\n## Manchester Code Proxy (MCP)"
+                                  APP_VERSION_STRING
+                                  " ("
+                                  APP_TIMESTAMP_STRING
+                                  ") command list ##\r\n"
+                                  "\r\n"
+                                  "help   - Prints the list of available commands\r\n"
+                                  "set    - Set a value in MCP configuration\r\n"
+                                  "get    - Get a value from MCP configuration\r\n"
+                                  "rxchg  - Raw Exchange Data command\r\n")
+                              );
 
     return NULL;
 }
---
+++ configuration/config_application.h
@@ -0,0 +1,14 @@
+/*
+ * config_application.h
+ *
+ *  Created on: Aug 10, 2021
+ *      Author: nenad
+ */
+
+#ifndef CONFIG_APPLICATION_H_
+#define CONFIG_APPLICATION_H_
+
+#define APP_VERSION_STRING      "V1.0"
+#define APP_TIMESTAMP_STRING    "2021-08-10"
+
+#endif /* CONFIG_APPLICATION_H_ */
